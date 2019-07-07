051317d0812bcb90a44f7fd06293f5ff1cbe2630
suoguangxu <suoguangxu@imooc.com>
2019-07-07 21:55:53 +0800

    使用redux+antd修改todolist

--- src/app.js
+++ src/app.js
@@ -1,29 +1,53 @@
 import React, { Component } from 'react'
 import { Input, Button, List } from 'antd'
-const data = [
-    'Racing car sprays burning fuel into crowd.',
-    'Japanese princess to wed commoner.',
-    'Australian walks 100km after outback crash.',
-    'Man charged over missing wedding girl.',
-    'Los Angeles battles huge wildfires.',
-];
+import { store } from './store';
+import { inputValue, addTodo, deletTodo } from './store/action'
+
 class TodoList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = store.getState();
+        this.handleStoreChange = this.handleStoreChange.bind(this);
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.addTodoItem = this.addTodoItem.bind(this);
+        store.subscribe(this.handleStoreChange);
+    }
     render () {
         return (
             <div style={{ marginTop: '10px', marginLeft: '10px' }}>
                 <Input
+                    value={this.state.inputValue}
                     placeholder='todo info'
                     style={{ width: '300px' }}
+                    onChange={this.handleInputChange}
                 ></Input>
-                <Button type="primary">提交</Button>
+                <Button
+                    type="primary"
+                    onClick={this.addTodoItem}
+                >提交</Button>
                 <List
                     style={{ width: '300px', marginTop: '10px' }}
                     bordered
-                    dataSource={data}
-                    renderItem={item => (<List.Item>{item}</List.Item>)}
+                    dataSource={this.state.todos}
+                    renderItem={(item, index) => (<List.Item onClick={this.deleteTodoItem.bind(this, index)}>{item}</List.Item>)}
                 />
             </div>
         )
+    }
+
+    handleStoreChange () {
+        this.setState(() => store.getState())
+    }
+    handleInputChange (e) {
+        store.dispatch(inputValue(e.target.value))
+    }
+    addTodoItem () {
+        let todo = this.state.inputValue
+        store.dispatch(addTodo(todo));
+        store.dispatch(inputValue(''));
+    }
+    deleteTodoItem (index) {
+        store.dispatch(deletTodo(index))
     }
 }
 
